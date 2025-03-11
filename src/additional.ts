3475ab085d91b8e43c2a392e6c1ef4feeae0b861
ninaraffaela <nina.r.ziegler@gmail.com>
2025-03-11 20:58:23 +0100

    eventlistener unlockUserInput2, unlockOutput

--- src/additional.ts
+++ src/additional.ts
@@ -7,5 +7,34 @@ const rb2024 = document.getElementById("2024") as HTMLInputElement;
 const rbGrundtarif = document.getElementById("rb-1person") as HTMLInputElement; 
 const rbSplittingtarif = document.getElementById("rb-2persons") as HTMLInputElement;
 
+
+rbSplittingtarif.addEventListener("click", unlockUserInput2);
+
+function unlockUserInput2 () {
+    if (rbSplittingtarif && userInput2) {
+        userInput2.classList.remove("hidden");
+        console.log("rb clicked");
+    }
+}
+
+rbGrundtarif.addEventListener("click", lockUserInput2);
+
+function lockUserInput2 () {
+    if (rbGrundtarif) {
+        userInput2.classList.add("hidden");
+    }
+}
+
 form?.addEventListener("submit", calculateTaxableIncome );
+
+function calculateTaxableIncome(event: Event) {
+    event.preventDefault();
+    console.log("geht");
+    if (output) {
+        output.classList.remove("hidden");
+        
+
+    }
+    
+}
 
